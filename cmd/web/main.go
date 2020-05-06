4d474783356a3ecfd6b20157920fb98e20e39036
Victor Hugo <hugo.hsn@gmail.com>
2020-05-06 10:58:22 -0400

    Add css, js and images

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Server is running on :4000")
 	error := http.ListenAndServe(":4000", mux)
 	log.Fatal(error)
