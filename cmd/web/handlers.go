2c29a70dbcc6bcf3d012ca8685e20e8e60e2c824
Victor Hugo <hugo.hsn@gmail.com>
2020-05-06 10:30:49 -0400

    Fixed template extension

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,8 +16,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	files := []string{
-		"./ui/html/home.page.html",
-		"./ui/html/base.layout.html",
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
