8637a7ea908e35c4afc4abf72af1ba779cb08a13
Victor Hugo <hugo.hsn@gmail.com>
2020-05-06 15:06:54 -0400

    Add Lshortfile at errorLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,7 +14,7 @@ func main() {
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
