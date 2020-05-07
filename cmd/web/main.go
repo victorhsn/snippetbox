76594fd35fb1b136cd9b2d0d857948ec931f0e07
Victor Hugo <hugo.hsn@gmail.com>
2020-05-07 17:12:43 -0400

    Create Pool Connection for MYSQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,10 +19,19 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "root:123456@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		infoLog:  infoLog,
@@ -33,8 +45,8 @@ func main() {
 	}
 
 	infoLog.Printf("Server is running on %s", *addr)
-	error := srv.ListenAndServe()
-	errorLog.Fatal(error)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 type neuteredFileSystem struct {
@@ -59,4 +71,18 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 		}
 	}
 	return f, nil
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
 }
